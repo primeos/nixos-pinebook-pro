3caaae1673e3012998fb905ddfd2c147a15eac1e
Michael Weiss <michael@primeos.dev>
2020-06-25 19:04:50 +0000

    build.sh: Simplify the script
    Let's assume that NIX_PATH is already setup correctly (the channel
    nixos-19.09 might not exist anyway).
    Note: The cross-compilation of the full image currently depends on [0].
    
    [0]: https://github.com/NixOS/nixpkgs/pull/88222

--- build.sh
+++ build.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-set -e
-set -u
 PS4=" $ "
+set -eux
 
-# Ugh, I would have liked to do it through a simpler `nix-build`, but  as this
-# needs to set `NIX_PATH` for use of `<nixpkgs/*>` imports, this is the better
-# way to go.
-
-set -x
-exec env -i NIXPKGS_ALLOW_UNFREE=1 NIX_PATH="nixpkgs=channel:nixos-19.09" nix-build \
-  system.nix -A config.system.build.sdImage "$@"
+export NIXPKGS_ALLOW_UNFREE=1
+nix-build system.nix -A config.system.build.sdImage "$@"
